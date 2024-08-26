2606a60cbef7d002585051ba64c72589d14951df
Erwan O <erwan.oger@additi.fr>
2024-08-26 19:56:07 +0200

    improve

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
     <h1>Home Page</h1>
-    <NuxtLink to="/v1">v1</NuxtLink>
-    <NuxtLink to="/benjamin">benjamin</NuxtLink>
-    <NuxtLink to="/no-repo">no-repo</NuxtLink>
+
+    <ul>
+        <li><NuxtLink to="/v1">v1</NuxtLink></li>
+        <li><NuxtLink to="/benjamin">benjamin</NuxtLink></li>
+        <li><NuxtLink to="/no-repo">no-repo</NuxtLink></li>
+    </ul>
 </template>
